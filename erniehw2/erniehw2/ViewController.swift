b49dd99191d6b23bf3c7b90f553d8688c73040f5
ErnieThompson <ernestleningrad@mail.ru>
2015-07-10 03:33:38 -0700

    uploaded Day 2 Homework

---
+++ erniehw2/erniehw2/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  erniehw2
+//
+//  Created by Ernie Thompson on 7/10/15.
+//  Copyright (c) 2015 Ernie Thompson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let stringArray:[String] = ["Well hey, this is a String.\nWhat could possibly be next?", "Gag me with a spoon!\nIt's another String!", "And a third...", "This is getting old...", "This is the last string in the array.\nBut I dare you to hit Next ;)"]
+    
+    var ind:Int = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myLabel.text = stringArray[ind]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func nextTapped(sender: AnyObject) {
+        
+        ind = ++ind % stringArray.count
+        
+        myLabel.text = stringArray[ind]
+    
+    }
+    
+    
+    @IBAction func previousTapped(sender: AnyObject) {
+        
+        
+        ind = (stringArray.count + (--ind % stringArray.count)) % stringArray.count
+       
+        myLabel.text = stringArray[ind]
+    
+    }
+    
+    @IBOutlet weak var myLabel: UILabel!
+
+}
+
